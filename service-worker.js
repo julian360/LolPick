407f26751db5f0345ee3e3a5a0ab40a8bfcc8ce4
julian360 <julian_lusuardi@hotmail.com>
2025-07-23 21:37:18 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'version-1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/images/Logo.png'
+];
+
+// Instalar el Service Worker y cachear los archivos principales
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interceptar las peticiones y servir desde el caché si está disponible
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Si la respuesta está en el caché, la retorna. Si no, busca en la red.
+        return response || fetch(event.request);
+      })
+  );
+});
